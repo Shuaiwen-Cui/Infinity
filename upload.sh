01d1833ed60392b24a5d62b68d65764ca5189188
Shuaiwen-Cui <SHUAIWEN001@e.ntu.edu.sg>
2023-12-16 19:49:36 +0800

    auto update Infinity

---
+++ upload.sh
@@ -0,0 +1,19 @@
+# usage: in git bash, input 'sh update.sh' - windows; or in terminal input './update.sh' and click approve git for windows pop up to run it
+# usage: in terminal, input 'bash update.sh' to run it
+# usage: note that the quotation marks should not be input into the terminal
+
+echo '--------upload files start--------'   
+# enter the target folder
+# cd ./
+
+# git init
+git add .
+git status
+git commit -m 'auto update Infinity'
+echo '--------commit successfully--------'
+
+# git push -f https://github.com/Shuaiwen-Cui/Infinity.git/ main
+git push -u https://github.com/Shuaiwen-Cui/Infinity.git/ main
+# git remote add origin https://github.com/Shuaiwen-Cui/Infinity.git/
+# git push -u origin main
+echo '--------push to GitHub successfully--------'
